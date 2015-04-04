a4bf2eb9131cf340c023086978da4986cdbf9875
ginsederp <ginsederp@gmail.com>
2015-04-05 06:36:04 +0800

    Added message box.

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,9 +1,20 @@
 // Script Dependencies:
 // jQuery
+// socket.io
 
-$(document).ready(function() {
+$(document).ready(function($) {
   setInterval(function() {
-    $('#subtitle').hide(300).fadeIn(1000);
-  }, 1300);
+    $('#subtitle').hide(300).fadeIn(9000);
+  }, 9300);
 
+  var $messageForm = $('#send_message')
+    , $messageBox = $('#message_box')
+    , $chatApp = $('#chat_app')
+    ;
+
+  $messageForm.submit(function(_event) {
+    _event.preventDefault();
+    socket.emit('send message', $messageBox.val());
+    $messageBox.val('');
+  });
 });
