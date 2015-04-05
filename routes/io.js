4d2f5e06dda6ffad331933006ca9e039222a44b3
ginsederp <ginsederp@gmail.com>
2015-04-05 08:30:18 +0800

    Renamed socket parameter to _socket.

--- routes/io.js
+++ routes/io.js
@@ -1,17 +1,17 @@
 var io = require('socket.io')();
 
-io.on('connection', function(socket) {
+io.on('connection', function(_socket) {
   var now = new Date();
   var connectionTime = new Date().toJSON();
 
-  var remoteAddress = socket.request.connection.remoteAddress;
-  var remotePort = socket.request.connection.remotePort;
+  var remoteAddress = _socket.request.connection.remoteAddress;
+  var remotePort = _socket.request.connection.remotePort;
 
-  console.log('['+ connectionTime + ']' + 'Connection detected from ' + remoteAddress + ':' + remotePort + ' with id ' + socket.id + '.');
-  socket.on('disconnect', function() {
+  console.log('['+ connectionTime + ']' + 'Connection detected from ' + remoteAddress + ':' + remotePort + ' with id ' + _socket.id + '.');
+  _socket.on('disconnect', function() {
     var now = new Date();
     var disconnectTime = new Date().toJSON();
-    console.log('['+ disconnectTime + ']' + 'Disconnection of id ' + socket.id + ' from address ' + remoteAddress + ':' + remotePort + '.');
+    console.log('['+ disconnectTime + ']' + 'Disconnection of id ' + _socket.id + ' from address ' + remoteAddress + ':' + remotePort + '.');
   });
 });
 
